2fbebfb6a5a8f010c74c4c5b0af5e4c6178b7e09
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-02-22 02:55:47 +0900

    feat: 임시 Activity

---
+++ app/src/main/java/com/hyundailogics/shupool/activity/TempActivity.kt
@@ -0,0 +1,14 @@
+package com.hyundailogics.shupool.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hyundailogics.shupool.R
+
+class TempActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_set_route)
+
+    }
+}
+
