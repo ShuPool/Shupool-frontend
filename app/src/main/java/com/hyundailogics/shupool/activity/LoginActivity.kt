dfce2ed2020c76c9dc46fe99bbd1ec67fb01ca90
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-02-22 22:47:35 +0900

    feat: LoginActivity 시작 및 전환

--- app/src/main/java/com/hyundailogics/shupool/activity/LoginActivity.kt
+++ app/src/main/java/com/hyundailogics/shupool/activity/LoginActivity.kt
@@ -1,18 +1,28 @@
 package com.hyundailogics.shupool.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.hyundailogics.shupool.R
+import com.hyundailogics.shupool.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var actionBar: ActionBar?
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SelectActivity::class.java)
+            startActivity(intent)
+        }
 
-        actionBar = supportActionBar
-        actionBar?.hide()
+//        var actionBar: ActionBar?
+//
+//        actionBar = supportActionBar
+//        actionBar?.hide()
+
     }
 }
