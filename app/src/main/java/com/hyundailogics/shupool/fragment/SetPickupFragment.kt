1d3f306d0fbb46cb771d10f99650dc154b78c546
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-02-22 23:57:09 +0900

    feat: 카풀 참여 요청하기, 뒤로가기 버튼 구현

--- app/src/main/java/com/hyundailogics/shupool/fragment/SetPickupFragment.kt
+++ app/src/main/java/com/hyundailogics/shupool/fragment/SetPickupFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hyundailogics.shupool.activity.TempActivity
+import com.hyundailogics.shupool.activity.CarpoolJoinActivity
 import com.hyundailogics.shupool.databinding.FragmentSetPickupBinding
 
 class SetPickupFragment : Fragment() {
@@ -17,9 +17,15 @@ class SetPickupFragment : Fragment() {
     ): View? {
         binding = FragmentSetPickupBinding.inflate(inflater, container, false)
 
+        binding.backBtn.setOnClickListener {
+            val activity = activity as CarpoolJoinActivity?
+            activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
+            activity?.supportFragmentManager?.popBackStack()
+        }
+
         binding.RequestButton.setOnClickListener {
-            val activity = activity as TempActivity?
-            activity?.onFragmentChanged(1)
+            val activity = activity as CarpoolJoinActivity?
+            activity?.finish()
         }
         return binding.root
     }
