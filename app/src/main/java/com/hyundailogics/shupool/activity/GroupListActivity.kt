77a53d7d5e405b1248ae15f2cc1263af686ca9ed
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-02-24 01:40:34 +0900

    feat: 그룹리스트-editText의 돋보기 vector 추가

---
+++ app/src/main/java/com/hyundailogics/shupool/activity/GroupListActivity.kt
@@ -0,0 +1,18 @@
+package com.hyundailogics.shupool.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.hyundailogics.shupool.R
+
+class GroupListActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mycarpool_before)
+
+        var actionBar: ActionBar?
+
+        actionBar = supportActionBar
+        actionBar?.hide()
+    }
+}
